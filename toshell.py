4396fa7440996dbf00bb8b5b3736db15cb4ee239
Your Name <you@example.com>
2024-05-14 18:07:14 +0200

    progress

--- toshell.py
+++ toshell.py
@@ -1,5 +1,6 @@
 from openai import OpenAI
 import subprocess
+import sys
 
 END = "I AM DONE!"
 
@@ -10,19 +11,25 @@ client = OpenAI(api_key=api_key)
 import os
 
 
-goal = "Check if there are any git changes, and if there are, commit them. Then push."
+goal = sys.argv[1]
 
 messages=[{"role": "system", "content": "You are a helpful assistant."},
-          {"role": "user", "content": "You are connected to a linux shell. Your whole response will be passed into it. You will get both the stdout and stderr streams back as a response. Once you see by the response, that you are done, respond with `"+END+"` to indicate that you are finished. Your goal is:  "+goal}]
-
+          {"role": "user", "content": """Your goal is:  {goal}. You are connected to a linux shell. Your whole response will be passed into it. 
+          You will get both the stdout and stderr streams back as a response. Use this, to interact with the shell, like a human would, to achieve your goal. 
+          Once you see by the response, that you are done, respond with `{end}` to indicate that you are finished. 
+          Prefer responding with single commands rather than with a script or series of commands. 
+          Do not try to use any interactive editors, like nano.""".format(goal=goal, end=END)}]
+print(messages)
 while True:
     response = client.chat.completions.create(model="gpt-4o", messages=messages)
     command = response.choices[0].message.content
     #print("Command raw", command)
     if (END == command): break
     command = command.lstrip("```sh\r\n").rstrip("```")
-    print("COMMAND>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\\n", command)
+    print("COMMAND>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n", command)
     messages.append({"role": "assistant", "content": response.choices[0].message.content})
+
+    if input("continue?(no)") == "no": break
 
     result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
 
@@ -31,7 +38,7 @@ while True:
     # Get the standard error (stderr), if needed
     errors = result.stderr
 
-    print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<OUTPUT\n:", output)
+    print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<OUTPUT:\n", output)
     
     if errors != "":
         print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Errors:\n", errors)
