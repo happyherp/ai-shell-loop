8a4083ae276ed5150a80578fc9085beb71617153
Your Name <you@example.com>
2024-05-15 13:02:03 +0200

    some formatting

--- toshell.py
+++ toshell.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 import subprocess
-import sys, json
+import sys, json, os
 
 END = "I AM DONE!"
 
@@ -8,7 +8,6 @@ with open("openai.key", 'r') as file:
     api_key = file.read()
 
 client = OpenAI(api_key=api_key)
-import os
 
 
 goal = sys.argv[1]
@@ -17,17 +16,17 @@ messages=[{"role": "system", "content": "You are a helpful assistant."},
           {"role": "user", "content": """Your goal is:  {goal}. You are connected to a linux shell. Your whole response will be passed into it. 
           You will get both the stdout and stderr streams back as a response. Use this, to interact with the shell, like a human would, to achieve your goal. 
           Once you see by the response, that you are done, respond with the command `{end}` to indicate that you are finished. 
-          Prefer responding with single commands rather than with a script or series of commands. 
           Do not try to use any interactive editors, like nano.
           Respond in a json format: 
           ```
           {{
             "plan": "<Describe how you plan to achieve the goal in plain english>",
+            "directory": "<the absolute path in which the command should be executed>"
             "command": "echo Hello World"
           }}
           ```
           
-          The content of "command" will be sent to the shell and you will receive the standart and error output. 
+          The content of "command" will be sent to the shell and you will receive the stdout and stderr. 
           
           """.format(goal=goal, end=END)}]
 while True:
@@ -39,8 +38,9 @@ while True:
     print(obj["plan"])
     command = obj["command"]
     #print("Command raw", command)
-    command = command.lstrip("```sh\r\n").rstrip("```")
-    print("COMMAND>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n", command)
+    command = "cd "+obj["directory"]+ " && " + command
+    print("COMMAND>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> in", obj["directory"])
+    print(command)
     messages.append({"role": "assistant", "content": response.choices[0].message.content})
     if (END == command): break
 
@@ -56,7 +56,7 @@ while True:
     responseContent = "stdout:\n"+output+"\n"
     if errors != "":
         print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Errors:\n", errors)
-        responseContent += "Errors: "+errors+"\n "
+        responseContent += "stderr:\n"+errors+"\n "
 
     messages.append({"role":"user", "content": responseContent})
 
