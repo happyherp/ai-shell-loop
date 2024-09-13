def36a4a79938f6041af9d5d07ef0af898554ad7
Your Name <you@example.com>
2024-09-13 10:06:05 +0200

    restructured code, added parameter to output schema to indicate termination.

--- toshell.py
+++ toshell.py
@@ -4,48 +4,23 @@ import subprocess, getpass
 import sys, json, os, copy
 from describe import describe
 from util import * 
-
-END = "I AM DONE!"
-
-client = createClient()
-
-
-goal = sys.argv[1]
-
 from pydantic import BaseModel, Field
+
+MODEL = "gpt-4o-2024-08-06"
 
 class ResponseContent(BaseModel):
     plan:str = Field(description="Describe how you plan to achieve the goal in plain english")
     directory:str = Field(description="the absolute path in which the command should be executed")
     command:str = Field(description="the shell to be executed to achieve the goal")
+    taskCompleted:bool = Field(description="""
+    Set this to `True`, if you confirmed that you have completed the task and no further actions are necessary.
+    If this is true, command should be `None`.
+    """)
 
 class Iteration(BaseModel):
     ai:ResponseContent
     userinput:str
     shellOutput:Optional[str] = None
-
-
-mainPrompt="""
-Your goal is:  {goal}. You are connected to a terminal. You 
-can pass commends to the shell to achieve the goal.
-You will get both the stdout and stderr streams back as a response. Use this to interact with the shell, to achieve your goal. 
-Once you see by the response, that you are done, respond with the command `{end}` to indicate that you are finished. 
-Do not try to use any interactive editors, like nano.
-
-Current Directory: {current_directory}
-Username: {username}
-
-{schema}
-
-The content of "command" will be sent to the shell and you will receive the stdout and stderr. 
-
-""".format(goal=goal, end=END, schema=describe(ResponseContent), current_directory=os.getcwd(),
-    username = getpass.getuser())
-
-messages=[systemMsg("You are a helpful assistant."), userMsg(mainPrompt)]
-
-total_tokens = 0
-iterations = []
 
 
 def fromIterations():
@@ -75,9 +50,37 @@ def reduceShelloutput(text):
         return text
         
 
+goal = sys.argv[1]
+
+
+mainPrompt="""
+Your goal is:  {goal}. You are connected to a terminal. You 
+can pass commends to the shell to achieve the goal.
+You will get both the stdout and stderr streams back as a response. Use this to interact with the shell, to achieve your goal. 
+Once you see by the response, that you are done, respong with taskCompleted=True to indicate that you are finished. 
+Do not try to use any interactive editors, like nano.
+
+Current Directory: {current_directory}
+Username: {username}
+
+{schema}
+
+The content of "command" will be sent to the shell and you will receive the stdout and stderr. 
+
+""".format(goal=goal, schema=describe(ResponseContent), current_directory=os.getcwd(),
+    username = getpass.getuser())
+
+messages=[systemMsg("You are a helpful assistant."), userMsg(mainPrompt)]
+
+total_tokens = 0
+iterations = []
+
+client = createClient()
+
+
 while True:
     #print("messages: ", fromIterations())
-    response = client.chat.completions.create(model="gpt-4o", messages=fromIterations(), 
+    response = client.chat.completions.create(model=MODEL, messages=fromIterations(), 
         response_format={ "type": "json_object" }
     )
     total_tokens += response.usage.total_tokens
@@ -88,7 +91,7 @@ while True:
     command = obj.command
     #print("Command raw", command)
     messages.append(assistantMsg(response.choices[0].message.content))
-    if (END == command): break
+    if (obj.taskCompleted): break
     command = "cd "+obj.directory+ " && " + command
     print("COMMAND>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> in", obj.directory)
     print(command)
