a2b34da71143e5da879d1a1807aaa27ea5f6fb5a
Carlos Freund <carlosfreund@gmail.com>
2024-09-15 11:40:13 +0200

    split main loop into multiple methods.

--- aishell.py
+++ aishell.py
@@ -15,10 +15,10 @@ def user_input_from_console(): return input("continue?(no, new command)")
 class ResponseContent(BaseModel):
     plan: str = Field(description="Describe how you plan to achieve the goal in plain english")
     directory: str = Field(description="the absolute path in which the command should be executed")
-    command: Optional[str] = Field(description="the shell to be executed to achieve the goal")
+    command: Optional[str] = Field(description="the shell command to be executed to achieve the goal. Set this to null if you are done.")
     task_completed: bool = Field(description="""
-    Set this to `True`, if you confirmed that you have completed the task and no further actions are necessary.
-    If this is True, command should be `None`. This field is not optional. 
+    Set this to `true`, if you confirmed that you have completed the task and no further actions are necessary.
+    If this is `true`, command should be `null`. This field is not optional. 
     """)
 
 
@@ -31,9 +31,9 @@ class Iteration(BaseModel):
 class AiShell:
     maxIterationsInHistory = 15
 
-    def __init__(self, mainPrompt: str, userInputSource=user_input_from_console):
-        self.mainPrompt = mainPrompt
-        self.userInputSource = userInputSource
+    def __init__(self, main_prompt: str, user_input_source=user_input_from_console):
+        self.main_prompt = main_prompt
+        self.user_input_source = user_input_source
         self.total_tokens = 0
         self.total_tokens = 0
         self.iterations = []
@@ -53,7 +53,7 @@ class AiShell:
         return content
 
     def build_messages(self):
-        messages = [system_msg("You are a helpful assistant."), user_msg(self.mainPrompt)]
+        messages = [system_msg("You are a helpful assistant."), user_msg(self.main_prompt)]
         if len(self.iterations) > 0:
             messages.append(user_msg(self.summarize_iterations()))
             last_iteration = self.iterations[-1]
@@ -72,43 +72,60 @@ class AiShell:
         else:
             return text
 
+    def run_once(self):
+        """
+        Sent the prompt to the AI and process its response.
+
+        Returns:
+            bool: True, if there should be another run. If the task is completed or the user cancelled it,
+            False is returned instead.
+        """
+        ai_response = self.call_ai()
+        print("PLAN>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n")
+        print(ai_response.plan)
+        command = ai_response.command
+        # print("Command raw", command)
+        if ai_response.task_completed: return False
+        print("COMMAND>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> in", ai_response.directory)
+        print(command)
+        shell_output = None
+        userinput = self.user_input_source()
+        if userinput == "no": return False
+        if userinput != "":
+            pass
+        else:
+            shell_output = self.execute_shell_command("cd " + ai_response.directory + " && " + command)
+        self.iterations.append(Iteration(ai=ai_response, userinput=userinput, shell_output=shell_output))
+        return True
+
     def loop(self):
 
-        while True:
-            response = self.client.chat.completions.create(
-                model=MODEL, messages=self.build_messages(), response_format={"type": "json_object"}
-            )
-            self.total_tokens += response.usage.total_tokens
-            print("Tokens: ", response.usage.total_tokens, "Total: ", self.total_tokens)
-            obj = ResponseContent.model_validate_json(response.choices[0].message.content)
-            print("PLAN>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n")
-            print(obj.plan)
-            command = obj.command
-            # print("Command raw", command)
-            if obj.task_completed: break
-            print("COMMAND>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> in", obj.directory)
-            print(command)
-            command = "cd " + obj.directory + " && " + command
-            shell_output = None
-            userinput = self.userInputSource()
-            if userinput == "no": break
-            if userinput != "":
-                pass
-            else:
-                result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-
-                output = result.stdout
-                errors = result.stderr
-
-                print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<OUTPUT:\n", output)
-
-                shell_output = "Return Code: " + str(result.returncode)
-                shell_output += "\nstdout:\n" + output + "\n"
-                if errors and errors != "":
-                    print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Errors:\n", errors)
-                    shell_output += "stderr:\n" + errors + "\n "
-            self.iterations.append(Iteration(ai=obj, userinput=userinput, shell_output=shell_output))
+        while self.run_once():
+            pass
         print("Loop finished. ")
+
+    def call_ai(self):
+        response = self.client.chat.completions.create(
+            model=MODEL, messages=self.build_messages(), response_format={"type": "json_object"}
+        )
+        self.total_tokens += response.usage.total_tokens
+        print("Tokens: ", response.usage.total_tokens, "Total: ", self.total_tokens)
+        return ResponseContent.model_validate_json(response.choices[0].message.content)
+
+    def execute_shell_command(self, command):
+        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+
+        output = result.stdout
+        errors = result.stderr
+
+        print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<OUTPUT:\n", output)
+
+        shell_output = "Return Code: " + str(result.returncode)
+        shell_output += "\nstdout:\n" + output + "\n"
+        if errors and errors != "":
+            print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Errors:\n", errors)
+            shell_output += "stderr:\n" + errors + "\n "
+        return shell_output
 
 
 def execute_goal(goal: str, user_input_source=user_input_from_console):
@@ -116,7 +133,7 @@ def execute_goal(goal: str, user_input_source=user_input_from_console):
     Your goal is:  {goal}. You are connected to a terminal. You 
     can pass commands to the shell to achieve the goal.
     You will get both the stdout and stderr streams back as a response. Use this to interact with the shell, to achieve your goal. 
-    Once you have confirmed, that you are done, respond with task_completed=True to indicate that you are finished. 
+    Once you have confirmed, that you are done, respond with task_completed=true to indicate that you are finished. 
     Do not try to use any interactive editors, like nano.
     
     Current Directory: {current_directory}
