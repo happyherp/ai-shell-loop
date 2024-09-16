3b097b6d8dfc59eff7a43029f95e9c45b30014f4
Carlos Freund <carlosfreund@gmail.com>
2024-09-16 20:11:59 +0200

    preparation for deployment to pypi

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="ai-do",
     version="0.1.0",
-    description="A tool to run shell commands iteratively using AI",
+    description="A command line tool that lets an AI do work for you.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     author="Carlos Freund",
     author_email="carlosfreund@gmail.com",
+    license="Apache License 2.0",
     packages=find_packages(),
     entry_points={
         'console_scripts': [
@@ -18,4 +21,10 @@ setup(
         'PyYAML==6.0.2'
     ],
     python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",   # For Linux systems
+        "Operating System :: MacOS :: MacOS X", # For macOS
+    ],
 )
