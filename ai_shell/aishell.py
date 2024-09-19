37c13c9977ce7576c6981c16f868075f85f3e00b
Carlos Freund <carlosfreund@gmail.com>
2024-09-19 18:32:31 +0200

    refactoring sorted method by call order renamed variables formatting

--- ai_shell/aishell.py
+++ ai_shell/aishell.py
@@ -7,7 +7,6 @@ from .logger import setup_logging
 log_file = setup_logging()
 print(f"Logging initialized. Log file: {log_file}")
 
-
 import copy
 import getpass
 import json
@@ -52,15 +51,17 @@ class AiResponse(BaseModel):
 
 class Iteration(BaseModel):
     command_plan: CommandPlan = Field(description="The CommandPlan that was executed.")
-    userinput: Optional[str] = Field(default = None, description=
-                                     "The input the user gave after seeing the command and plan. "
-                                     + "Empty string means the user was ok with the command. null means no user input was given. "
-                                     + "Another string means the user did not want to execute the command. "
-                                     + "The string is the reason why. It is to be considered as an amendment or extension of the original goal.")
-    missing_commands: Optional[List[str]] = Field(default = None, description=
-                                                  "Commands that where not available and the shell. For that reason, the command was not executed.")
-    shell_output: Optional[str] = Field(default= None, description="If the command of the CommandPlan was executed, this contains the stdout, stderr and result code.")
-    check_shell_output: Optional[str] = Field(default= None, description="If the check_command of the CommandPlan was executed, this contains the stdout, stderr and result code.")
+    userinput: Optional[str] = Field(default=None, description=
+    "The input the user gave after seeing the command and plan. "
+    + "Empty string means the user was ok with the command. null means no user input was given. "
+    + "Another string means the user did not want to execute the command. "
+    + "The string is the reason why. It is to be considered as an amendment or extension of the original goal.")
+    missing_commands: Optional[List[str]] = Field(default=None, description=
+    "Commands that where not available and the shell. For that reason, the command was not executed.")
+    shell_output: Optional[str] = Field(default=None, description=
+    "If the command of the CommandPlan was executed, this contains the stdout, stderr and result code.")
+    check_shell_output: Optional[str] = Field(default=None, description=
+    "If the check_command of the CommandPlan was executed, this contains the stdout, stderr and result code.")
 
 
 class AiShell:
@@ -78,26 +79,66 @@ class AiShell:
         self.unavailable_commands = set()
         self.sudo_password = None
 
+    def loop(self):
+        while self.run_once():
+            pass
+        print("Loop finished. ")
 
-    def summarize_iterations(self):
-        content = "These are the commands that have previously been executed as json. \n"
-        content += "They have the following schema: \n"
-        iteration_schema = Iteration.model_json_schema()
-        # Modify Iteration schema to reference CommandPlan via $ref
-        # The schema of CommandPlan is already part of the system prompt.
-        iteration_schema['properties']['command_plan'] = {"$ref": "#/definitions/CommandPlan"}
-        del iteration_schema["$defs"]["CommandPlan"]
-        content += codeblock(json.dumps(iteration_schema, indent=0)) + "\n"
-        if len(self.iterations) > AiShell.maxIterationsInHistory:
-            content += str(len(self.iterations) - AiShell.maxIterationsInHistory) + " iterations skipped.\n "
+    def run_once(self):
+        """
+        Sent the prompt to the AI and process its response.
 
-        content += "Previous iterations:\n'''json\n[\n"
-        for i in self.iterations[-AiShell.maxIterationsInHistory:]:
-            small = copy.deepcopy(i)
-            small.shell_output = self.reduce_shelloutput(small.shell_output)
-            content += small.model_dump_json(indent=0) + ",\n"
-        content += "]\n'''\n"
-        return content
+        Returns:
+            bool: True, if there should be another run. If the task is completed or the user cancelled it,
+            False is returned instead.
+        """
+        ai_response = self.call_ai()
+        if ai_response.task_completed:
+            print("AI completed its task.")
+            return False
+        if not ai_response.command_options:
+            print("Ai did not provide a single command.")
+            return False
+        self.check_commands_availability(ai_response.command_options)
+        for command_plan in ai_response.command_options:
+            self.print_command(command_plan)
+            missing_commands = self.unavailable_commands & set(command_plan.used_commands)
+            if missing_commands:
+                print("Missing commands:", missing_commands)
+                self.iterations.append(Iteration(command_plan=command_plan, missing_commands=missing_commands))
+                continue
+            userinput = self.user_input_source()
+            if userinput == "no":
+                return False
+            if userinput != "":
+                # do not execute shell command. Userinput will be presented to AI on next run.
+                self.iterations.append(Iteration(command_plan=command_plan, userinput=userinput))
+            else:
+                command_process = self.execute_shell_command(
+                    "cd " + command_plan.directory + " && " + command_plan.command)
+                self.print_shell_output(command_process)
+                output_as_string = self.shell_output_to_string(command_process)
+                if command_process.returncode == 0:
+                    print("Checking if command was successful: ", command_plan.check_command)
+                    check_process = self.execute_shell_command(
+                        "cd " + command_plan.directory + " && " + command_plan.check_command)
+                    self.print_shell_output(check_process)
+                    check_output_as_string = self.shell_output_to_string(check_process)
+                    self.iterations.append(
+                        Iteration(command_plan=command_plan, userinput=userinput, shell_output=output_as_string,
+                                  check_shell_output=check_output_as_string))
+                else:
+                    self.iterations.append(
+                        Iteration(command_plan=command_plan, userinput=userinput, shell_output=output_as_string))
+        return True
+
+    def call_ai(self) -> AiResponse:
+        response = self.client.chat.completions.create(
+            model=MODEL, messages=self.build_messages(), response_format={"type": "json_object"}
+        )
+        self.total_tokens += response.usage.total_tokens
+        print("Tokens: ", response.usage.total_tokens, "Total: ", self.total_tokens)
+        return AiResponse.model_validate_json(response.choices[0].message.content)
 
     def build_messages(self):
         messages = [
@@ -141,83 +182,60 @@ The content of "command" will be sent to the shell and you will receive the stdo
 If the return code of the command is 0, the content of "check_command" will also be executed and you will receive the stdout and stderr. 
 
 """.format(schema=describe(AiResponse), current_directory=os.getcwd(),
-                       username=getpass.getuser())
+           username=getpass.getuser())
 
-    def reduce_shelloutput(self, text):
+    def summarize_iterations(self):
+        content = "These are the commands that have previously been executed as json. \n"
+        content += "They have the following schema: \n"
+        iteration_schema = Iteration.model_json_schema()
+        # Modify Iteration schema to reference CommandPlan via $ref
+        # The schema of CommandPlan is already part of the system prompt.
+        iteration_schema['properties']['command_plan'] = {"$ref": "#/definitions/CommandPlan"}
+        del iteration_schema["$defs"]["CommandPlan"]
+        content += codeblock(json.dumps(iteration_schema, indent=0)) + "\n"
+        if len(self.iterations) > AiShell.maxIterationsInHistory:
+            content += str(len(self.iterations) - AiShell.maxIterationsInHistory) + " iterations skipped.\n "
+
+        content += "Previous iterations:\n'''json\n[\n"
+        for i in self.iterations[-AiShell.maxIterationsInHistory:]:
+            small = copy.deepcopy(i)
+            small.shell_output = self.reduce_shell_output(small.shell_output)
+            content += small.model_dump_json(indent=0) + ",\n"
+        content += "]\n'''\n"
+        return content
+
+    def reduce_shell_output(self, text):
         if not text: return text
-        maxSize = 80 * 10
-        extraChars = len(text) - maxSize
-        if extraChars > 0:
-            return text[:maxSize // 2] + "<< {0} chars skipped>>".format(extraChars) + text[-maxSize // 2:]
+        max_size = 80 * 10
+        extra_chars = len(text) - max_size
+        if extra_chars > 0:
+            return text[:max_size // 2] + "<< {0} chars skipped>>".format(extra_chars) + text[-max_size // 2:]
         else:
             return text
 
-    def run_once(self):
-        """
-        Sent the prompt to the AI and process its response.
+    def get_command_availability(self):
+        return "confirmed available commands: {}\n unavailable commands: {}".format(
+            self.available_commands, self.unavailable_commands)
 
-        Returns:
-            bool: True, if there should be another run. If the task is completed or the user cancelled it,
-            False is returned instead.
-        """
-        ai_response = self.call_ai()
-        if ai_response.task_completed:
-            print("AI completed its task.")
-            return False
-        if not ai_response.command_options:
-            print("Ai did not provide a single command.")
-            return False
-        self.check_commands_availability(ai_response.command_options)
-        for command_plan in ai_response.command_options:
-            self.printCommand(command_plan)
-            missing_commands = self.unavailable_commands & set(command_plan.used_commands)
-            if missing_commands:
-                print("Missing commands:", missing_commands)
-                self.iterations.append(Iteration(command_plan=command_plan, missing_commands=missing_commands))
-                continue
-            userinput = self.user_input_source()
-            if userinput == "no":
-                return False
-            if userinput != "":
-                # do not execute shell command. Userinput will be presented to ai on next run.
-                self.iterations.append(Iteration(command_plan=command_plan, userinput=userinput))
-            else:
-                command_process = self.execute_shell_command(
-                    "cd " + command_plan.directory + " && " + command_plan.command)
-                self.print_shell_output(command_process)
-                output_as_string = self.shell_output_to_string(command_process)
-                if command_process.returncode == 0:
-                    print("Checking if command was successful: ", command_plan.check_command)
-                    check_process = self.execute_shell_command(
-                        "cd " + command_plan.directory + " && " + command_plan.check_command)
-                    self.print_shell_output(check_process)
-                    check_output_as_string = self.shell_output_to_string(check_process)
-                    self.iterations.append(
-                        Iteration(command_plan=command_plan, userinput=userinput, shell_output=output_as_string,
-                                  check_shell_output=check_output_as_string))
-                else:
-                    self.iterations.append(
-                        Iteration(command_plan=command_plan, userinput=userinput, shell_output=output_as_string))
-        return True
-
-    def printCommand(self, command_plan):
+    def print_command(self, command_plan):
         print("PLAN>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
         print(command_plan.plan)
         print("COMMAND>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> in", command_plan.directory)
         print(command_plan.command)
 
-    def loop(self):
-        while self.run_once():
-            pass
-        print("Loop finished. ")
-
-    def call_ai(self) -> AiResponse:
-        response = self.client.chat.completions.create(
-            model=MODEL, messages=self.build_messages(), response_format={"type": "json_object"}
-        )
-        self.total_tokens += response.usage.total_tokens
-        print("Tokens: ", response.usage.total_tokens, "Total: ", self.total_tokens)
-        return AiResponse.model_validate_json(response.choices[0].message.content)
+    def check_commands_availability(self, command_options: List[CommandPlan]):
+        """
+        Checks if the commands used in the command-options are available on the current machine.
+        It will update self.available_commands and self.unavailable_commands accordingly.
+        """
+        all_used_commands = {cmd for plan in command_options for cmd in plan.used_commands}
+        for command in all_used_commands:
+            if shutil.which(command) is not None:
+                self.available_commands.add(command)
+                self.unavailable_commands.discard(command)
+            else:
+                self.available_commands.discard(command)
+                self.unavailable_commands.add(command)
 
     def execute_shell_command(self, command) -> subprocess.CompletedProcess[str]:
         logging.info(f"Executing shell command: {command}")
@@ -226,7 +244,8 @@ If the return code of the command is 0, the content of "check_command" will also
                 self.sudo_password = getpass.getpass("Please enter your sudo password: ")
             command = command.replace("sudo", f"echo {self.sudo_password} | sudo -S")
 
-        completed_process = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+        completed_process = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True,
+                                           shell=True)
         logging.info(f"Command executed with return code {completed_process.returncode}")
         logging.debug(f"stdout: {completed_process.stdout}")
         logging.debug(f"stderr: {completed_process.stderr}")
@@ -249,33 +268,11 @@ If the return code of the command is 0, the content of "check_command" will also
             shell_output += "stderr:\n" + codeblock(errors)
         return shell_output
 
-    def check_commands_availability(self, command_options: List[CommandPlan]):
-        """
-        Checks if the commands used in the command-options are available on the current machine.
-        It will update self.available_commands and self.unavailable_commands accordingly.
-        """
-        all_used_commands = {cmd for plan in command_options for cmd in plan.used_commands}
-        for command in all_used_commands:
-            if shutil.which(command) is not None:
-                self.available_commands.add(command)
-                self.unavailable_commands.discard(command)
-            else:
-                self.available_commands.discard(command)
-                self.unavailable_commands.add(command)
-
-    def get_command_availability(self):
-        return "confirmed available commands: {}\n unavailable commands: {}".format(self.available_commands,
-                                                                                    self.unavailable_commands)
-
-
-def execute_goal(goal: str, user_input_source=user_input_from_console):
-    aishell = AiShell(goal, user_input_source)
-    aishell.loop()
-
 
 def main():
     goal = sys.argv[1] if len(sys.argv) > 1 else "No goal provided"
-    execute_goal(goal)
+    aishell = AiShell(goal)
+    aishell.loop()
 
 
 if __name__ == "__main__":
