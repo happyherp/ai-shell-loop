7708f16a5aa80c12fac95ef72322be2a47af6d32
Your Name <you@example.com>
2025-04-01 08:56:35 +0200

    updated openai version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         ],
     },
     install_requires=[
-        'openai==1.45.0',
+        'openai==1.70.0',
         'pydantic==2.9.1',
         'PyYAML==6.0.2',
         'appdirs==1.4.4'
